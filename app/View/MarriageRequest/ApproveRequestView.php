6113dc178c3b80796fadec6c33f0c184032143d1
LUTSi15 <kholidahmad02@gmail.com>
2023-06-15 18:14:01 +0800

    15/6

--- app/View/MarriageRequest/ApproveRequestView.php
+++ app/View/MarriageRequest/ApproveRequestView.php
@@ -22,6 +22,12 @@ if (!isset($_SESSION['currentUserIC'])) {
 
     //Sidebar Active path
     $_SESSION['route'] = 'viewProfile';
+
+    // Retrieve the serialized and URL-encoded data from the URL parameter
+    $applicantEncodedData = $_GET['applicantInfo'];
+
+    // Decode the URL-encoded data and unserialize it
+    $decodedApplicantData = unserialize(urldecode($applicantEncodedData));
 }
 
 
