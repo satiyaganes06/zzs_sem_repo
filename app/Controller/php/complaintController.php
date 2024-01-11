4742ae7594f6d65f275a2ef52263a4fa4ecb5e60
zhikang168 <nzk820@gmail.com>
2024-01-12 05:04:53 +0800

    complaint
    adding function in complaintController

--- app/Controller/php/complaintController.php
+++ app/Controller/php/complaintController.php
@@ -21,7 +21,15 @@
         return $complaintInfo;
     }
 
+    public function addComplaintInfo($complaintId, $consultationId, $adminId, $applicantIC, $purpose, $challenges, $solution, $complaintDate, $complaintStatus) {
+        $result = $this->complaintModel->addComplaintInfo($complaintId, $consultationId, $adminId, $applicantIC, $purpose, $challenges, $solution, $complaintDate, $complaintStatus);
 
+        if ($result) {
+            echo "Complaint information added successfully!";
+        } else {
+            echo "Failed to add complaint information.";
+        }
+    }
 
 }
 ?>
